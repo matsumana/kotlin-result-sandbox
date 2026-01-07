1f692fb2f235fa7ffadaa7b7ffebabdadae7b95f
Manabu Matsuzaki <matsumana@gmail.com>
2026-01-07 17:12:40 +0900

    refactor: delete the unused DTOs

--- sandbox-domain/src/main/kotlin/com/example/sandbox/domain/dto/UserResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sandbox.domain.dto
-
-data class UserResponseDto(
-    val id: Int,
-    val name: String,
-    val position: String,
-    val mailAddress: String
-)
--- sandbox-domain/src/main/kotlin/com/example/sandbox/domain/dto/UserUpdateRequestDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sandbox.domain.dto
-
-data class UserUpdateRequestDto(
-    val name: String,
-    val position: String,
-    val mailAddress: String
-)
