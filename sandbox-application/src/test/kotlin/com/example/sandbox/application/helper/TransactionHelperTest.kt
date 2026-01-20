04fdb6f2cf3d1b6aaa9fadbb7086967a4e39f4f1
Manabu Matsuzaki <matsumana@gmail.com>
2026-01-20 16:17:59 +0900

    test: enhance TransactionHelperTest with nested test classes for better organization

--- sandbox-application/src/test/kotlin/com/example/sandbox/application/helper/TransactionHelperTest.kt
+++ sandbox-application/src/test/kotlin/com/example/sandbox/application/helper/TransactionHelperTest.kt
@@ -1,9 +1,11 @@
 package com.example.sandbox.application.helper
 
+import com.github.michaelbull.result.Err
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
@@ -20,47 +22,75 @@ class TransactionHelperTest(
         jdbcTemplate.execute("DELETE FROM test_entity")
     }
 
-    @Test
-    fun `data should be persisted when no exception is thrown`() {
-        val entity = TestEntity(id = 1, name = "Test")
+    @Nested
+    inner class SuccessTest {
+        @Test
+        fun `data should be persisted when no exception is thrown`() {
+            val entity = TestEntity(id = 1, name = "Test")
 
-        transactionHelper.binding(
-            onException = { it }
-        ) {
-            insertEntity(entity)
+            transactionHelper.binding(
+                onException = { it }
+            ) {
+                insertEntity(entity)
+            }
+
+            val found = findEntityById(entity.id)
+            found.shouldNotBeNull()
+            found.name.shouldBe(entity.name)
         }
-
-        val found = findEntityById(entity.id)
-        found.shouldNotBeNull()
-        found.name.shouldBe(entity.name)
     }
 
-    @Test
-    fun `transaction should rollback when RuntimeException is thrown`() {
-        val entity = TestEntity(id = 1, name = "Test")
+    @Nested
+    inner class RuntimeExceptionTest {
+        @Test
+        fun `transaction should rollback when RuntimeException is thrown`() {
+            val entity = TestEntity(id = 1, name = "Test")
 
-        transactionHelper.binding(
-            onException = { it }
-        ) {
-            insertEntity(entity)
-            throw RuntimeException("Test RuntimeException")
+            transactionHelper.binding(
+                onException = { it }
+            ) {
+                insertEntity(entity)
+                throw RuntimeException("Test RuntimeException")
+            }
+
+            findEntityById(entity.id).shouldBeNull()
         }
-
-        findEntityById(entity.id).shouldBeNull()
     }
 
-    @Test
-    fun `transaction should rollback when checked exception is thrown`() {
-        val entity = TestEntity(id = 1, name = "Test")
+    @Nested
+    inner class CheckedExceptionTest {
+        @Test
+        fun `transaction should rollback when checked exception is thrown`() {
+            val entity = TestEntity(id = 1, name = "Test")
 
-        transactionHelper.binding(
-            onException = { it }
-        ) {
-            insertEntity(entity)
-            throw IOException("Test checked exception")
+            transactionHelper.binding(
+                onException = { it }
+            ) {
+                insertEntity(entity)
+                throw IOException("Test checked exception")
+            }
+
+            findEntityById(entity.id).shouldBeNull()
         }
+    }
 
-        findEntityById(entity.id).shouldBeNull()
+    @Nested
+    inner class ErrTest {
+        @Test
+        fun `transaction should rollback when Err is returned`() {
+            val entity = TestEntity(id = 1, name = "Test")
+
+            transactionHelper.binding(
+                onException = { it }
+            ) {
+                insertEntity(entity)
+                Err(
+                    RuntimeException("Test error")
+                ).bind()
+            }
+
+            findEntityById(entity.id).shouldBeNull()
+        }
     }
 
     private fun insertEntity(entity: TestEntity) {
